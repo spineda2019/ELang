2bb868241340fbef3473d8679eb52956da738b73
spineda2019 <spineda.wpi.alum@gmail.com>
2024-03-29 08:33:09 -0400

    rename file

---
+++ src/elang_interpreter.rs
@@ -0,0 +1,27 @@
+enum CommunicationProtocol {
+    Udp,
+    Tcp,
+}
+
+enum MessageFormat {
+    Raw,
+    Scpi,
+}
+
+pub struct ElangInterpreter<'a> {
+    port: Option<u16>,
+    address: Option<&'a str>,
+    message_format: Option<MessageFormat>,
+    communication_protocol: Option<CommunicationProtocol>,
+}
+
+impl<'a> ElangInterpreter<'a> {
+    pub fn new() -> Self {
+        Self {
+            port: None,
+            address: None,
+            message_format: None,
+            communication_protocol: None,
+        }
+    }
+}
