3effef0ac13f2e960c776c1055351f05a4956e2f
spineda2019 <spineda.wpi.alum@gmail.com>
2024-03-29 08:51:24 -0400

    feat: cleanly get filename argument and pass to interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod elang_interpreter;
 
 use elang_interpreter::ElangInterpreter;
-use std::ops::Index;
+use std::io::{Error, ErrorKind};
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Hello, world!");
     let args: Vec<String> = std::env::args().collect();
 
     let interpreter: ElangInterpreter<'_> = ElangInterpreter::new();
 
-    match args.len() {
-        1 => todo!(),
-        _ => todo!(),
+    match (args.len(), args.get(1)) {
+        (1, _) => interpreter.launch_interactive_shell(),
+        (x, Some(file_name)) if x > 1 => interpreter.interpret_file(file_name),
+        _ => {
+            const ERROR_MESSAGE: &str = "FATAL: Args in unknown state!";
+            return Err(Error::new(ErrorKind::InvalidInput, ERROR_MESSAGE));
+        }
     }
+
+    Ok(())
 }
