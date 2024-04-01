241cafbe040d71664653880f640c06c19f30a14f
spineda2019 <spineda.wpi.alum@gmail.com>
2024-04-01 13:50:21 -0400

    explicitly look for argc of 2

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@
  */
 
 mod elang_interpreter;
+mod keywords;
 
 use elang_interpreter::ElangInterpreter;
 use std::io::{Error, ErrorKind};
@@ -27,7 +28,7 @@ fn main() -> Result<(), Error> {
 
     match (args.len(), args.get(1)) {
         (1, _) => interpreter.launch_interactive_shell(),
-        (x, Some(file_name)) if x > 1 => interpreter.interpret_file(file_name),
+        (x, Some(file_name)) if x == 2 => interpreter.interpret_file(file_name)?,
         _ => {
             const ERROR_MESSAGE: &str = "FATAL: Args in unknown state!";
             return Err(Error::new(ErrorKind::InvalidInput, ERROR_MESSAGE));
